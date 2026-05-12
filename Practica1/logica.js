f935e6532431d5d37926532cc9c479c3649f79ac
Joan Osvaldo Higuera Gallardo <josved4@gmail.com>
2026-05-11 19:01:12 -0600

    parte 1 de la practica 1

---
+++ Practica1/logica.js
@@ -0,0 +1,3 @@
+    let nombre = prompt("¿Cuál es tu nombre?") //let es declarar una variable.  prompt es para pedir datos al usuario
+    console.log("Hola " + nombre) // console.log es para mostrar mensajes en la consola del navegador  
+    document.write("Hola " + nombre) // document.write es para mostrar mensajes en la página web modificando directamente documento HTML
