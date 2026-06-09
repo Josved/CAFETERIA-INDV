efb7ef0a33cfffd5c086f6a5dac367df20f9c93d
Joan Osvaldo Higuera Gallardo <josved4@gmail.com>
2026-06-09 11:48:30 -0600

    Agregar Practica5 React Native

---
+++ Practica5/components/Perfil.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+export const Perfil = () => {
+  return (
+    <View>
+      <Text>Nombre: Joan Osvaldo Higuera Gallardo</Text>
+      <Text>Carrera: Ingenieria en Sistemas Computacionales</Text>
+      <Text>Materia: Programacion Web</Text>
+      <Text>Cuatrimestre: S201</Text>
+    </View>
+  );
+};
